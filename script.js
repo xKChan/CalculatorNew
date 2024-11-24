5afb67c7e8d9f24aaa9cb98c0a824074a323ce88
kc <xkylechan@gmail.com>
2024-11-24 00:54:52 -0700

    added data attributes to the numbers.  Now JS can get the value of that number pressed with the getAttributes function.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const numSelected = document.querySelectorAll('.num');
+
 function add(a, b) {
   return a + b;
 }
@@ -13,3 +15,20 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+// function operate(a, b, opperand) {
+//   if (opperand == ) {
+
+//   }
+// }
+
+function getNum() {
+  numSelected.forEach(num => {
+    num.addEventListener('click', () => {
+      let numPicked = num.getAttribute('data-num');
+      console.log(numPicked);
+    });
+  });
+}
+
+getNum();
