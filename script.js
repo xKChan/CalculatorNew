60ef5270969525b08671afe48f609dcb93cca271
kc <xkylechan@gmail.com>
2025-02-17 23:07:38 -0700

    calcuclator dispalys numbers, and functions when user chooses and operrator and computes for answer.  if numbers have decimals, it rounds to 2 deciaml places

--- script.js
+++ script.js
@@ -3,26 +3,29 @@ const opperandSelected = document.querySelectorAll('.opperand');
 const equalSelected = document.querySelector('.equal');
 const decimalSelected = document.querySelector('.decimal');
 const displayedNumber = document.querySelector('.numberDisplay');
+const topDisplayedSelected = document.querySelector('.topDisplay');
+const bottomDisplayedSelected = document.querySelector('.bottomDisplay');
 const clearSelected = document.querySelector('.clear');
 
 let a = 0;
-let b = 0;
+let z = 0;
 let opperand = '';
+let displayAnswer = 0;
 
 function add(a, b) {
-  return parseInt(a) + parseInt(b);
+  return parseFloat(a) + parseFloat(b);
 }
 
 function subtract(a, b) {
-  return a - b;
+  return parseFloat(a) - parseFloat(b);
 }
 
 function multiply(a, b) {
-  return a * b;
+  return parseFloat(a) * parseFloat(b);
 }
 
 function divide(a, b) {
-  return a / b;
+  return parseFloat(a) / parseFloat(b);
 }
 
 function operate(a, b, opperand) {
@@ -39,8 +42,57 @@ function operate(a, b, opperand) {
 
 function clear() {
   clearSelected.addEventListener('click', () => {
-    displayedNumber.textContent = '';
+    topDisplayedSelected.textContent = '';
+    bottomDisplayedSelected.textContent = '';
+    a = 0;
+    z = 0;
+    opperand = '';
+  });
+}
+
+function getInput() {
+  numSelected.forEach(num => {
+    num.addEventListener('click', () => {
+      bottomDisplayedSelected.append(num.getAttribute('data-num'));
+    });
+  });
+}
+
+function getDecimal() {
+  decimalSelected.addEventListener('click', () => {
+    bottomDisplayedSelected.append('.');
+  });
+}
+
+function getOpperand() {
+  opperandSelected.forEach(opp => {
+    opp.addEventListener('click', () => {
+      a = bottomDisplayedSelected.textContent;
+      opperand = opp.getAttribute('data-opp');
+      topDisplayedSelected.textContent = a;
+      bottomDisplayedSelected.textContent = '';
+    });
+  });
+}
+
+function getAnswer() {
+  equalSelected.addEventListener('click', () => {
+    let num1 = a;
+    let num2 = bottomDisplayedSelected.textContent;
+    answer = operate(num1, num2, opperand);
+    if (answer % 1 != 0) {
+      displayAnswer = answer.toFixed(2);
+    } else {
+      displayAnswer = answer;
+    }
+    console.log(displayAnswer);
+    bottomDisplayedSelected.textContent = displayAnswer;
+    topDisplayedSelected.textContent = '';
   });
 }
 
 clear();
+getDecimal();
+getInput();
+getOpperand();
+getAnswer();
